6c163f742f49567f39d5c9465532dd3112b77003
mmulka <mmulka@innominds.com>
2016-03-22 15:14:59 +0530

    chbgs

--- src/SA/Utilities/MailNG.java
+++ src/SA/Utilities/MailNG.java
@@ -6,6 +6,7 @@ import SA.SeleniumLib.Common;
 public class MailNG {
 
 	public static void main(String[] args) throws Exception {
+		logApp.logIntilize();
 //		MailTrigger.reportmail();
 		Common.Regression_Run_Backup();
 		logApp.logger.info("Regression Results Backup Taken Successfully.");
